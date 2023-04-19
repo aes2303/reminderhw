675bec9eff3f5a631c4bd27374f04df76562abd6
aes2303 <aes2303@oita.kosen-ac.jp>
2023-04-19 10:32:22 +0900

    1回目はnotifyされないとかいうゴミコードを修正した

--- main.py
+++ main.py
@@ -168,10 +168,12 @@ context = True
 async def periodic_func():
     global first_time, context
     now = datetime.now(timezone(timedelta(hours=9))).strftime('%H:%M')
+    print(now)
     if now == '00:00' or now == '12:00':
         if first_time:
             first_time = False
             context = now == '00:00'
+            await task()
             return
         if context and now == '12:00' or not context and now == '00:00':
             await task()
